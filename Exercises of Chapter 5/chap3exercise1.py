7ae14d7a63c70205fc761d05e2941f7886bfb44f
christophersunith <sunith.christopher@gmail.com>
2023-05-12 20:07:28 +0100

    updated exercise 1 of chapter 5

--- Exercises of Chapter 5/chap3exercise1.py
+++ Exercises of Chapter 5/chap3exercise1.py
@@ -1,7 +1,23 @@
+#writing a program to calculate the area of a triangle
+
+#Title of the Program
+print("\nProgram to calculate the area of a triangle \n")
+
+#Ask user to enter the base as integer
 base = int(input("Enter the base(cm): "))
-print("You entered: " + str(base) + "cms")
+
+#prints the base that the user has entered (notice the type casting)
+print("\nYou entered: " + str(base) + "cms\n")
+
+#Ask user to enter the height as string
 height = int(input("Enter the base(cm): "))
-print("You entered: " + str(height) + "cms")
+
+#prints the height as string (notice the type casting)
+print("\nYou entered: " + str(height) + "cms\n")
+
+#function to calculate the area
 area = ((base * height) / 2)
-print(str(area) + "cm\u00b2")
+
+#Prints the result as string (notice the type casting)
+print("Area = " + str(area) + "cm\u00b2 \n")
 
